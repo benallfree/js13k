8da8fb9b59ea0597f4067d6fa0457c00b3c18c6d
Ben Allfree <ben@benallfree.com>
2025-06-14 18:26:29 -0700

    status bar to common

--- src/components/StatusBar.ts
+++
@@ -1,6 +0,0 @@
-import van, { State } from 'vanjs-core'
-
-const { div } = van.tags
-
-export const StatusBar = (statusMessage: State<string>, statusVisible: State<boolean>) =>
-  div({ class: () => `status-bar ${statusVisible.val ? 'visible' : ''}` }, () => statusMessage.val)
