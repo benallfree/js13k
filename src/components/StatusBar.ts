b7208df3c7288f08d6722ad690f64e05df0d84ce
Ben Allfree <ben@benallfree.com>
2025-06-14 17:04:18 -0700

    Refactor StatusBar component styles and visibility logic to improve animation effects and enhance user experience.

--- src/components/StatusBar.ts
+++ src/components/StatusBar.ts
@@ -3,4 +3,4 @@ import van, { State } from 'vanjs-core'
 const { div } = van.tags
 
 export const StatusBar = (statusMessage: State<string>, statusVisible: State<boolean>) =>
-  div({ class: 'status-bar' }, () => statusMessage.val)
+  div({ class: () => `status-bar ${statusVisible.val ? 'visible' : ''}` }, () => statusMessage.val)
