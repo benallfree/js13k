19eb4306b3295a2744a51380759ea052dd30f9c9
Ben Allfree <ben@benallfree.com>
2025-06-14 20:44:54 -0700

    add router

---
+++ src/common/router/link.ts
@@ -0,0 +1,32 @@
+import van from 'vanjs-core'
+import { _routerBasename, _routerPathname } from './_state'
+
+interface LinkProps extends Partial<HTMLAnchorElement> {
+  replace?: boolean
+}
+
+export function Link({ replace, ...props }: LinkProps, ...children: (HTMLElement | string)[]) {
+  const { onclick, href, ...rest } = props as HTMLAnchorElement
+
+  const anchor = van.tags.a(
+    {
+      ...rest,
+      href,
+      onclick: (e: MouseEvent) => {
+        e.preventDefault()
+
+        if (!replace) window.history.pushState({}, '', _routerBasename.val + href)
+        else window.history.replaceState({}, '', _routerBasename.val + href)
+
+        // Update the global state of the router to trigger the Router
+        if (href) _routerPathname.val = _routerBasename.val + href
+
+        // Call original anchor onclick, if defined
+        onclick?.bind(anchor)?.(e)
+      },
+    },
+    ...children
+  )
+
+  return anchor
+}
