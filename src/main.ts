a9bf3eec2a8ae0893e5c7e1a1e72be5835581e8d
Ben Allfree <ben@benallfree.com>
2025-06-22 05:15:36 -0700

    Import 'div' from common tags and update app initialization in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import van from 'vanjs-core'
 import { App } from './App'
+import { div } from './common/tags'
 import './global.css'
 
+// van.add(document.getElementById('app')!, () => div(`Van13k`))
 van.add(document.getElementById('app')!, App())
