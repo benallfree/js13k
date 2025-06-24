7eeadcb00df6e837375253bbcbd5e9f52f5fffce
Ben Allfree <ben@benallfree.com>
2025-06-23 20:40:30 -0700

    clean up bootloader

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import van from 'vanjs-core'
 import { App } from './App'
-import { div } from './common/tags'
 import './global.css'
 
-// van.add(document.getElementById('app')!, () => div(`Van13k`))
 van.add(document.getElementById('app')!, App())
