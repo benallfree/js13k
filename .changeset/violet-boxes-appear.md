7dfda3bb415f6ae4263ac83c5255048e6c74153b
Ben Allfree <ben@benallfree.com>
2025-06-27 10:11:21 -0700

    Refactor type definitions in classify.ts to improve component and attribute handling

---
+++ .changeset/violet-boxes-appear.md
@@ -0,0 +1,5 @@
+---
+'van13k': patch
+---
+
+Refactor type definitions in classify.ts to improve component and attribute handling
--- src/van13k/util/classify.ts
+++ src/van13k/util/classify.ts
@@ -1,6 +1,6 @@
-export type VanJsClass = string | (() => string)
-export type VanJsAttribute = string | (() => string)
+export type VanJsComponent = (...args: any[]) => HTMLElement
+export type VanJsAttributeValue = string | ((...args: any[]) => string)
 
-export const classify = (...args: VanJsClass[]) => {
+export const classify = (...args: VanJsAttributeValue[]) => {
   return { class: () => args.map((arg) => (typeof arg === 'string' ? arg : arg())).join(' ') }
 }
