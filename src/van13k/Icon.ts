6e2cf093e39cfe1417be7843a450655d105ba590
Ben Allfree <ben@benallfree.com>
2025-07-03 21:38:07 -0700

    feat: enhance Icon component to support routing with optional path prop

--- src/van13k/Icon.ts
+++ src/van13k/Icon.ts
@@ -1,20 +1,28 @@
-import { Button, ButtonSize, ButtonVariant, classify } from '@van13k'
+import { Button, ButtonSize, ButtonVariant, classify, Router } from '@van13k'
 import styles from './Button.module.css'
 
 interface IconProps {
   onClick: () => void
   active?: boolean
   class?: string
+  path?: string
   children: any
 }
 
-export const Icon = ({ onClick, active = false, class: className = '', children }: IconProps) => {
-  return Button({
-    onClick,
-    variant: active ? ButtonVariant.Primary : ButtonVariant.Cancel,
-    size: ButtonSize.Medium,
-    ...classify(styles.btn, styles.btnIcon, className),
-    isActive: active,
-    children,
-  })
+export const Icon = ({ onClick, active = false, class: className = '', path, children }: IconProps) => {
+  const b = () =>
+    Button({
+      onClick,
+      variant: active ? ButtonVariant.Primary : ButtonVariant.Cancel,
+      size: ButtonSize.Medium,
+      ...classify(styles.btn, styles.btnIcon, className),
+      isActive: active,
+      children,
+    })
+  if (path) {
+    return Router({
+      routes: [{ path, component: b }],
+    })
+  }
+  return b()
 }
