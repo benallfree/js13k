b93b70822ce7acbf9ad21cb81766b3d95bdad66f
Ben Allfree <ben@benallfree.com>
2025-07-06 10:35:29 -0700

    Export VanJsComponent and VanJsChild

---
+++ .changeset/gentle-eels-change.md
@@ -0,0 +1,5 @@
+---
+'van13k': minor
+---
+
+Export VanJsComponent and VanJsChild
---
+++ src/van13k/van.ts
@@ -0,0 +1,7 @@
+import van, { BindingFunc, State, type ChildDom } from 'vanjs-core'
+
+export type VanJsComponent = BindingFunc
+export type VanJsChild = ChildDom
+
+export { van }
+export type { State }
